4ff20f6b75e9a540648f3e1e18240d3f43ac6c7a
RazielMartelus96 <razielmartelus@curiosity-core.com>
2023-07-18 15:49:53 +0100

    InitialCommit, package-info.java created with TODO item.

---
+++ src/main/java/io/curiositycore/curiousclasses/mythicmobs/package-info.java
@@ -0,0 +1,3 @@
+//TODO this package will integrate the skills system from the mythic mobs plugin (and crucible) to allow more diverse
+//     skills to be added to the archetypes & account for custom items that should only be used by certain archetypes.
+package io.curiositycore.curiousclasses.mythicmobs;
