596040aaba0e975ca4b98fc91afb032d319250e8
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-07-20 20:57:15 +0100

    InitialCommit, package-info.java created with TODO item.

---
+++ src/main/java/io/curiositycore/curiousclasses/messages/package-info.java
@@ -0,0 +1,3 @@
+//TODO This package will be responsible for all the standard messages of the plugin, including errors, player messages,
+//     utility messages etc. Please refer to empty classes for guidance on requirements.
+package io.curiositycore.curiousclasses.messages;
